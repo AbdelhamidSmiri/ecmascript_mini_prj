4c7851bb71924f4b476ccd7e94ef32aca07dd0ed
Abdelhamid smiri <abde-smr@hotmail.fr>
2024-03-13 13:17:29 +0000

    Finaliser le mini projet en ECMAScript

--- js/myscript.js
+++ js/myscript.js
@@ -1,32 +1,78 @@
 import Student from "./student.js";
+const MAX_NOTE = 20
 
+let filtersetting = {
+    'column': 'id',
+    'desc': false
+}
+const getage = (date) => (new Date()).getFullYear() - new Date(date).getFullYear()
+const isAdmit = (note) => note >= 10
+let isFirstLoad = true;
 const dispalystudents = async function () {
     return Student.allstudents().then(function (response) {
-        return response.map((student) => {
-            const { id, name, date, note } = student
 
+
+
+
+        response.sort((a, b) => {
+            const isNumber = typeof a[filtersetting.column] === 'number'
+            // SORT NUMBERS
+            if (isNumber) {
+                if (filtersetting.desc) {
+                    return b[filtersetting.column] - (a[filtersetting.column])
+                }
+
+                return a[filtersetting.column] - (b[filtersetting.column])
+            }
+            // SORT STRINGS
+            if (filtersetting.desc) {
+                return b[filtersetting.column].localeCompare(a[filtersetting.column])
+            }
+
+            return a[filtersetting.column].localeCompare(b[filtersetting.column])
+        })
+        return response.map((data) => {
+            const { id, name, date, note } = data
+            let age = getage(date)
             return (`<tr>
                 <td>${id}</td>
                 <td>${name}</td>
-                <td>${date}</td>
-                <td>${note}</td>
-                <td><button class='btn btn-danger btn-sm'>Supprimer</button></td>
+                <td>${age} ans</td>
+                <td><span class="badge rounded-pill text-bg-${isAdmit(note) ? 'success' : 'danger'} ">${note} / ${MAX_NOTE}</span> </td>
+                <td><button class='btn btn-danger btn-sm btn_rmv_st' data-id='${id}'>Supprimer</button></td>
               </tr>`)
         })
     })
 
 }
 
+
 const addStudent = function (event) {
-    event.preventDefault() // kt3ni annuler lia dakchi li kadir donc ghay annuler lia submit dyal form bhala dart return false
+    event.preventDefault()
     const [nom, date, note] = document.querySelectorAll("#nom, #date, #note")
-    console.log(nom.value, date, note)
+    console.log(nom.value, date.value, note.value)
     const student = new Student(nom.value, date.value, note.value)
     student.addStudent()
 }
-const renderStudent = function () {
+function add_student_listener() {
+    let btn_add_st = document.querySelector("#addstudent")
+    btn_add_st.addEventListener('click', (event) => {
+        addStudent(event)
+    })
+}
+
+const removestudent = (id) => {
+    Student.removeStudent(id)
+}
+
+const renderStudent = async function () {
+    add_student_listener()
     const tbody = document.querySelector(".list_students")
-    dispalystudents().then(data => tbody.innerHTML = data.join(' ')) //had join bach thayad dik , li chaybatni 
+    await dispalystudents().then(data => tbody.innerHTML = data.join(' '))
+    if (isFirstLoad)
+        init()
+
+    isFirstLoad = false
 
 }
 const init = function () {
@@ -34,13 +80,48 @@ const init = function () {
     btn_reload.addEventListener('click', () => {
         renderStudent()
     })
-    let btn_add_st = document.querySelector("#addstudent")
-    btn_add_st.addEventListener('click', (event) => {
-        addStudent(event)
+    let btn_rmv_st = document.querySelectorAll(".btn_rmv_st")
+
+    btn_rmv_st.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            removestudent(btn.dataset.id)
+        })
+    })
+    let btn_sort_st = document.querySelectorAll(".sort-element")
+    btn_sort_st.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            filtersetting.column = btn.dataset.column
+            clearSpanArrow()
+            console.log(filtersetting.desc)
+            renderSort(filtersetting.column)
+
+        })
     })
 }
 
+const renderSort = (column) => {
+    if (filtersetting.column === column) {
+        const element = document.querySelector(".sort-element[data-column='" + column + "'] span")
+        element.innerHTML = `<button class='btn fw-bold' onclick="sortDirection()">${filtersetting.desc ? '&#8593;' : '&#8595;'}</button>`
+    }
+}
+const clearSpanArrow = () => {
+    document.querySelectorAll(".sort-element span").forEach(span => {
+        span.innerHTML = ''
+    })
+}
+window.sortDirection = function () {
+    filtersetting.desc = !filtersetting.desc
+    clearSpanArrow()
+    renderSort(filtersetting.column)
+    renderStudent()
+}
 
 
-init()
+renderSort('id')
+renderSort('name')
+renderSort('date')
+renderSort('note')
 renderStudent()
+
+
--- js/student.js
+++ js/student.js
@@ -1,13 +1,14 @@
 import { url } from "./constants.js"
-
+let lastId = 0;
 export default class Student {
-    static nextId = 4;
-    constructor(nom, age, note) {
-        this.id = Student.nextId++;
+
+    constructor(nom, date, note) {
         this.nom = nom
-        this.age = age
+        this.date = date
         this.note = note
     }
+
+
 
     static allstudents = async function () {
         const response = await fetch(url)
@@ -15,19 +16,47 @@ export default class Student {
         return students
     }
 
+    static fetchLastId = async function () {
+        try {
+            const response = await fetch(url); // Assuming this URL fetches the list of students
+            const students = await response.json();
+            if (students.length > 0) {
+                lastId = students.length + 1;
+            }
+        } catch (error) {
+            console.error('Error fetching students:', error);
+        }
+        return lastId;
+    };
+
     addStudent = async function () {
+        let lastid = await Student.fetchLastId()
         const response = await fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                id: this.id,
+                id: lastid,
                 name: this.nom,
-                date: this.age,
-                note: this.note
+                date: this.date,
+                note: parseFloat(this.note)
             })
         })
+        return response
+    }
+
+    static removeStudent = async function (id) {
+        const response = "";
+        if (confirm("Êtes-vous sûr de supprimer l'étudiant ?") == true) {
+            response = await fetch(url + "/" + id, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })
+        }
+
         return response
     }
 
